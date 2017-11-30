260e9a4a457023e7c2e4893ea455f365ecec6d0e
Bui Ngoc Chien <bui@tagbangers.co.jp>
2017-11-30 19:30:18 +0900

    commit

---
+++ SimpleServerDemo.java
@@ -0,0 +1,94 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServerDemo {
+
+    public static void main(String args[]) throws IOException {
+
+        ServerSocket listener = null;
+
+        System.out.println("Server is waiting to accept user...");
+        int clientNumber = 0;
+
+
+        try {
+            listener = new ServerSocket(7777);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        try {
+            while (true) {
+
+                // accept the request from client
+
+                Socket socketOfServer = listener.accept();
+                new ServiceThread(socketOfServer, clientNumber++).start();
+            }
+        } finally {
+            listener.close();
+        }
+
+    }
+
+    private static void log(String message) {
+        System.out.println(message);
+    }
+
+    private static class ServiceThread extends Thread {
+
+        private int clientNumber;
+        private Socket socketOfServer;
+
+        public ServiceThread(Socket socketOfServer, int clientNumber) {
+            this.clientNumber = clientNumber;
+            this.socketOfServer = socketOfServer;
+
+            // Log
+            log("New connection with client# " + this.clientNumber + " at " + socketOfServer);
+        }
+
+        @Override
+        public void run() {
+
+            try {
+
+
+                // create thread input and output at Server.
+                BufferedReader is = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+                BufferedWriter os = new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream()));
+
+                while (true) {
+                    //read output from client
+                    String line = is.readLine();
+
+                    // write data
+                    os.write(">> " + line);
+                    os.newLine();
+                    // send to client
+                    os.flush();
+
+                    // end thread
+                    if (line.equals("QUIT")) {
+                        os.write(">> OK");
+                        os.newLine();
+                        os.flush();
+                        break;
+                    }
+                }
+
+            } catch (IOException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+    }
+}
